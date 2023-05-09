29c7d222c0769216e8b7425ca4f5e571dad0b75e
RMAELILLA <ronioryzamae12@gmail.com>
2023-05-10 07:54:55 +0800

    checking process

---
+++ calculator_math_operations.py
@@ -0,0 +1,10 @@
+# ask user to choose math operations between "Addition", "Subtraction", "Multiplication", or "Division"
+# if addition
+    # then ask for 2 number
+    # display result
+# if subtraction
+    # then ask for 2 number
+# if multiplication
+    # then ask for 2 number
+# if division
+    # then ask for 2 number
