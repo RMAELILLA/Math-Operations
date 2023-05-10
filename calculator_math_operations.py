cdf0c6d8d080d833c12d5d3741b1b06de0c510f9
RMAELILLA <ronioryzamae12@gmail.com>
2023-05-10 08:30:18 +0800

    if addition

--- calculator_math_operations.py
+++ calculator_math_operations.py
@@ -1,9 +1,11 @@
 # ask user to choose math operations between "Addition", "Subtraction", "Multiplication", or "Division"
 math_operator = input("Good day! Please choose what math operation you need; 'Addition', 'Subtraction', 'Multiplication' or 'Division'")
 # evaluate the chosen math operations
-print(len(math_operator))
+for i in range(len(math_operator)):
     # if input is "Addition"
+    if math_operator == "addition":
         # ask for 2 numbers
+        input("Please enter 'First Number': ")
         # display result
     # if input is "Subtraction"
         # ask for 2 numbers
