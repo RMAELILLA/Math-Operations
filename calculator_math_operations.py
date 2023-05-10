0055fb16234c3940878aa0e9ed864858d3099aab
RMAELILLA <ronioryzamae12@gmail.com>
2023-05-10 08:14:38 +0800

    evaluate input math operator

--- calculator_math_operations.py
+++ calculator_math_operations.py
@@ -1,8 +1,9 @@
 # ask user to choose math operations between "Addition", "Subtraction", "Multiplication", or "Division"
-with input("Good day! Please choose what math operation you need; 'Addition', 'Subtraction', 'Multiplication' or 'Division'"):
-    for
-    # evaluate the chosen math operations
+with input("Good day! Please choose what math operation you need; 'Addition', 'Subtraction', 'Multiplication' or 'Division'") as math_operator:
+    for i in range(len(math_operator)):
+        # evaluate the chosen math operations
         # if input is "Addition"
+        if math_operator[i] == "addition":
             # ask for 2 numbers
             # display result
         # if input is "Subtraction"
@@ -14,8 +15,8 @@ with input("Good day! Please choose what math operation you need; 'Addition', 'S
         # if input is "Division"
             # ask for 2 numbers
             # display result
-    # ----------------------------------------------
-    # ask user if wants to input again
-        # if yes
-        # if no
-            # display appreciation
+# ----------------------------------------------
+# ask user if wants to input again
+    # if yes
+    # if no
+       # display appreciation
