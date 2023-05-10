7884da890db1904214b352c98a8b8665329384c3
RMAELILLA <ronioryzamae12@gmail.com>
2023-05-10 08:19:41 +0800

    change function

--- calculator_math_operations.py
+++ calculator_math_operations.py
@@ -1,20 +1,21 @@
 # ask user to choose math operations between "Addition", "Subtraction", "Multiplication", or "Division"
-with input("Good day! Please choose what math operation you need; 'Addition', 'Subtraction', 'Multiplication' or 'Division'") as math_operator:
-    for i in range(len(math_operator)):
-        # evaluate the chosen math operations
-        # if input is "Addition"
-        if math_operator[i] == "addition":
+input("Good day! Please choose what math operation you need; 'Addition', 'Subtraction', 'Multiplication' or 'Division'")
+for i in range(len(math_operator)):
+    # evaluate the chosen math operations
+    # if input is "Addition"
+    if math_operator[i] == "addition":
+        # ask for 2 numbers
+        input("please enter First number: ")
+        # display result
+    # if input is "Subtraction"
+        # ask for 2 numbers
+        # display result
+    # if input is "Multiplication"
             # ask for 2 numbers
-            # display result
-        # if input is "Subtraction"
-            # ask for 2 numbers
-            # display result
-        # if input is "Multiplication"
-            # ask for 2 numbers
-            # display result
-        # if input is "Division"
-            # ask for 2 numbers
-            # display result
+        # display result
+    # if input is "Division"
+        # ask for 2 numbers
+        # display result
 # ----------------------------------------------
 # ask user if wants to input again
     # if yes
