83ec1431bb7821937be4cda61dfc01e13279bcd3
RMAELILLA <ronioryzamae12@gmail.com>
2023-05-11 21:40:19 +0800

    enclose with math operator options

--- calculator_math_operations.py
+++ calculator_math_operations.py
@@ -5,37 +5,39 @@ def calculator_math_operations():
         math_operator = input("Good day! Please choose what math operation you need: 'Addition', 'Subtraction', 'Multiplication' or 'Division': ")
         # evaluate the chosen math operations
         math_operator = math_operator.lower()
-        # ask for 2 numbers
-        first_number = int(input("Please enter 'First Number': "))
-        second_number = int(input("Please enter 'Second 'Number': "))
+        
         # if input is "Addition"
-        if math_operator == "addition":
-            # calculate
-            addition = first_number + second_number
-            # display result
-            print("The sum is: ", addition)
-        # if input is "Subtraction"
-        elif math_operator == "subtraction":
-            # calculate
-            subtraction = first_number - second_number
-            # display result
-            print("The difference is: ", subtraction)
-        # if input is "Multiplication"
-        elif math_operator == "multiplication":
-            # calculate
-            multiplication = first_number * second_number
-            # display result
-            print("The product is: ", multiplication)
-        # if input is "Division"
-        elif math_operator == "division":
-            try:
+        if math_operator in == ["addition", "subtraction", "multiplication", "division"]:
+             # ask for 2 numbers
+            first_number = int(input("Please enter 'First Number': "))
+            second_number = int(input("Please enter 'Second 'Number': "))
+            if math_operator == "addition":
                 # calculate
-                division = first_number // second_number
-            except ZeroDivisionError:
-                print("Cannot process division, your divisor is zero")
-            # display result
-            else:
-                print("The quotient is: ", division)
+                addition = first_number + second_number
+                # display result
+                print("The sum is: ", addition)
+            # if input is "Subtraction"
+            elif math_operator == "subtraction":
+                # calculate
+                subtraction = first_number - second_number
+                # display result
+                print("The difference is: ", subtraction)
+            # if input is "Multiplication"
+            elif math_operator == "multiplication":
+                # calculate
+                multiplication = first_number * second_number
+                # display result
+                print("The product is: ", multiplication)
+            # if input is "Division"
+            elif math_operator == "division":
+                try:
+                    # calculate
+                    division = first_number // second_number
+                except ZeroDivisionError:
+                    print("Cannot process division, your divisor is zero")
+                # display result
+                else:
+                    print("The quotient is: ", division)
         # else
         else:
             print("I don't understant your input, please choose one only in the four math operators.")
