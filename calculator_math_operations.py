9400751c314669a9ad0908a5c7c7e22060ce2aac
RMAELILLA <129654335+RMAELILLA@users.noreply.github.com>
2023-05-12 11:12:14 +0800

    final output

--- calculator_math_operations.py
+++ calculator_math_operations.py
@@ -8,39 +8,40 @@ def calculator_math_operations():
         math_operator = math_operator.lower()
         # if input is in math operator
         if math_operator in ["addition", "subtraction", "multiplication", "division"]:
-             # ask for 2 numbers
-            first_number = int(input("Please enter 'First Number': "))
-            second_number = int(input("Please enter 'Second 'Number': "))
             try:
-                if math_operator == "addition":
-                    # calculate
-                    addition = first_number + second_number
-                    # display result
-                    print("The sum is: ", addition)
-                # if input is "Subtraction"
-                elif math_operator == "subtraction":
-                    # calculate
-                    subtraction = first_number - second_number
-                    # display result
-                    print("The difference is: ", subtraction)
-                # if input is "Multiplication"
-                elif math_operator == "multiplication":
-                    # calculate
-                    multiplication = first_number * second_number
-                    # display result
-                    print("The product is: ", multiplication)
-                # if input is "Division"
-                elif math_operator == "division":
-                    try:
-                        # calculate
-                        division = first_number // second_number
-                    except ZeroDivisionError:
-                        print("Cannot process division, your divisor is zero")
-                    # display result
-                    else:
-                        print("The quotient is: ", division)
+                first_number = int(input("Please enter 'First Number': "))
+                second_number = int(input("Please enter 'Second 'Number': "))
             except ValueError:
                 print("Field cannot include non-integer or non-numerical values or be blank.")
+                break
+            if math_operator == "addition":
+                # calculate
+                addition = first_number + second_number
+                # display result
+                print("The sum is: ", addition)
+            # if input is "Subtraction"
+            elif math_operator == "subtraction":
+                # calculate
+                subtraction = first_number - second_number
+                # display result
+                print("The difference is: ", subtraction)
+            # if input is "Multiplication"
+            elif math_operator == "multiplication":
+                # calculate
+                multiplication = first_number * second_number
+                # display result
+                print("The product is: ", multiplication)
+            # if input is "Division"
+            elif math_operator == "division":
+                try:
+                    # calculate
+                    division = first_number // second_number
+                except ZeroDivisionError:
+                    print("Cannot process division, your divisor is zero")
+                # display result
+                else:
+                    print("The quotient is: ", division)
+        
         # else
         else:
             print("I don't understant your input, please choose one only in the four math operators.")
@@ -48,14 +49,15 @@ def calculator_math_operations():
         
         # decision statement
         math_operator_2 = (input("Do you want to calculate again? y/n: "))
-        if math_operator_2 == "y":
+        if math_operator_2.lower == "y":
             continue
-        elif math_operator_2 == "n":
+        elif math_operator_2.lower == "n":
             print("Thank you for using this program. Have a Good day!")
             break
         else:
             if not type(math_operator_2) == "y" "n":
-                raise TypeError("Please choose y/n only")
-
+                print("Please choose 'y' only if yes or 'n' only if no")
+            break
+            
 # ----------------------------------------------
 calculator_math_operations()
